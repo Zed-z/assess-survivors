614f10bf1f33dde417d2c4ddbd4c0eeca9998e7a
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-07-27 19:05:05 +0200

    Cooler textures

--- scenes/settings_menu/settings_menu.gd
+++ scenes/settings_menu/settings_menu.gd
@@ -1,4 +1,22 @@
+@tool
 extends Control
+
+
+@onready var tab_bar: TabBar = $TabBar
+@onready var content_panel: Control = $Panel  # or whatever node contains your pages
+
+func _populate_tabs():
+	tab_bar.clear_tabs()
+	for child in content_panel.get_children():
+		tab_bar.add_tab(child.name)
+
+func _on_tab_selected(tab_index: int):
+	_show_tab(tab_index)
+
+func _show_tab(index: int):
+	var children = content_panel.get_children()
+	for i in range(len(children)):
+		children[i].visible = (i == index)
 
 
 var close_callback: Callable
@@ -10,17 +28,22 @@ func close() -> void:
 
 
 func tab_prev() -> void:
-	if $TabContainer.current_tab > 0:
-		$TabContainer.current_tab -= 1
+	if $TabBar.current_tab > 0:
+		$TabBar.current_tab -= 1
 
 
 func tab_next() -> void:
-	if $TabContainer.current_tab < $TabContainer.get_tab_count() - 1:
-		$TabContainer.current_tab += 1
+	if $TabBar.current_tab < $TabBar.get_tab_count() - 1:
+		$TabBar.current_tab += 1
 
 
 func _ready() -> void:
-	$TabContainer/General/VBoxContainer/Button.grab_focus()
+
+	tab_bar.tab_selected.connect(_on_tab_selected)
+	_populate_tabs()
+	_show_tab(0)
+	
+	$Panel/General/VBoxContainer/Button.grab_focus()
 
 
 func _on_close_button_pressed() -> void:
