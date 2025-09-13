c8c27c1ccbc57d234764ed37010da814459df009
igorekkedziorek <126686402+igorekkedziorek@users.noreply.github.com>
2025-09-13 21:59:43 +0200

    criterion_manager

---
+++ globals/assess_criterion_manager/criterion_variable_val.gd
@@ -0,0 +1,14 @@
+extends Criterion
+
+func preferred_val():
+	right_bound = point_list[-2].x 
+	point_list[-2].x = (left_bound + point_list[-2].x) / 2
+	#pewniak się robi jako (MIN_value + pewniak) / 2
+
+func preferred_lottery():
+	left_bound = point_list[-2].x
+	point_list[-2].x = (point_list[-2].x + right_bound) / 2
+	#pewniak się robi jako (pewniak + CUR_MAX_value) / 2
+func set_bound():
+		left_bound = point_list[-3].x
+		right_bound = point_list[-1].x
