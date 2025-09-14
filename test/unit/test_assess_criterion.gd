004e1f03139c717de936a32c8742a773ee52a640
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-09-14 20:05:52 +0200

    Test cleanup

--- test/unit/test_assess_criterion.gd
+++ test/unit/test_assess_criterion.gd
@@ -1,27 +1,49 @@
 extends GutTest
 
+
 @onready var criterion_script: GDScript = preload("res://globals/assess_criterion_manager/criteria/assess_criterion_variable_probability.gd")
 var exp: Array[Vector2]
+
+
+func assert_almost_eq_vector2_array(actual: Array[Vector2], expected: Array[Vector2], delta: float):
+	assert_eq(actual.size(), expected.size())
+	for i in range(actual.size()):
+		assert_almost_eq(actual[i].x, expected[i].x, delta)
+		assert_almost_eq(actual[i].y, expected[i].y, delta)
+
 
 func test_point_append():
 	var criterion: AssessCriterion = criterion_script.new()
 
-	assert_eq(criterion.point_list, [Vector2(0.0, 0.0),Vector2(10.0, 1.0)])
+	assert_almost_eq_vector2_array(
+		criterion.point_list,
+		[Vector2(0.0, 0.0),Vector2(10.0, 1.0)],
+		0.01)
 
 	criterion.do_point_append()
 
-	assert_eq(criterion.point_list, [Vector2(0.0, 0.0),Vector2(10.0, 0.5),Vector2(20.0, 1.0)])
+	assert_almost_eq_vector2_array(
+		criterion.point_list,
+		[Vector2(0.0, 0.0),Vector2(10.0, 0.5),Vector2(20.0, 1.0)],
+		0.01)
 
 	criterion.do_point_append()
 
-	exp = [Vector2(0.0, 0.0),Vector2(10.0, 0.33),Vector2(20.0, 0.66),Vector2(30.0, 1.0)]
-	for i in range(criterion.point_list.size()):
-		assert_almost_eq(criterion.point_list[i].x, exp[i].x, 0.01)
-		assert_almost_eq(criterion.point_list[i].y, exp[i].y, 0.01)
+	assert_almost_eq_vector2_array(
+		criterion.point_list,
+		[Vector2(0.0, 0.0),Vector2(10.0, 0.33),Vector2(20.0, 0.66),Vector2(30.0, 1.0)],
+		0.01)
 
 	criterion.do_point_append()
 
-	exp = [Vector2(0.0, 0.0),Vector2(10.0, 0.25),Vector2(20.0, 0.50),Vector2(30.0, 0.75),Vector2(40.0, 1.0)]
-	for i in range(criterion.point_list.size()):
-		assert_almost_eq(criterion.point_list[i].x, exp[i].x, 0.01)
-		assert_almost_eq(criterion.point_list[i].y, exp[i].y, 0.01)
+	assert_almost_eq_vector2_array(
+		criterion.point_list,
+		[Vector2(0.0, 0.0),Vector2(10.0, 0.25),Vector2(20.0, 0.50),Vector2(30.0, 0.75),Vector2(40.0, 1.0)],
+		0.01)
+
+	criterion.do_point_append()
+
+	assert_almost_eq_vector2_array(
+		criterion.point_list,
+		[Vector2(0.0, 0.0),Vector2(10.0, 0.2),Vector2(20.0, 0.4),Vector2(30.0, 0.6),Vector2(40.0, 0.8),Vector2(50.0, 1.0)],
+		0.01)
