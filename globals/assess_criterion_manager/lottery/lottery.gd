3bbdeccb68608eda59fc0c0c4456686a50d93a10
igorekkedziorek <126686402+igorekkedziorek@users.noreply.github.com>
2025-09-16 13:33:55 +0200

    lottery

--- globals/assess_criterion_manager/lottery/lottery.gd
+++ globals/assess_criterion_manager/lottery/lottery.gd
@@ -7,6 +7,11 @@ class_name Lottery
 @export var loss_probability : float
 @export var loss_value: float
 
+func _init(win_val, win_prob, loss_val) -> void:
+	win_value = win_val
+	win_probability = win_prob
+	loss_value = loss_val
+	loss_probability = 1 - win_prob
+
 func _to_string() -> String:
-	return "(" 
-	#+ "," + + String(loss_value) + ")"
+	return "("+"%.02f" % win_value +","+"%.02f" % win_probability +","+"%.02f" % loss_value +","+ ")"
