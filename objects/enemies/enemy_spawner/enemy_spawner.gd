8a66970603f369f9fe2399d39e5ae5d0b433dc7a
shamotte <123325697+shamotte@users.noreply.github.com>
2025-09-18 20:42:59 +0200

    Update enemy_spawner.gd

--- objects/enemies/enemy_spawner/enemy_spawner.gd
+++ objects/enemies/enemy_spawner/enemy_spawner.gd
@@ -3,14 +3,17 @@ extends Node2D
 @export var player: Player
 
 @export_group("spawn parameters")
-@export var min_radius: float =100
-@export var max_radius: float =200
+@export var min_radius: float=100
+@export var max_radius: float=200
 @export var enemy: PackedScene
+
+var enemies_array: SwapbackArray
 
 
 func _process(_delta: float) -> void:
 	var radius = randf_range(min_radius,max_radius)
 	var offset = Vector2(radius,0).rotated(randf_range(0,3.14 * 2))
-	var e:Node2D= enemy.instantiate()
+	var e:Node2D =         enemy.instantiate()
+
 	add_child(e)
 	e.global_position = player.global_position + offset
