d53a0a28d7ee553763f31333f94e150ed32aad8c
shamotte <123325697+shamotte@users.noreply.github.com>
2025-09-20 20:48:16 +0200

    working on it

---
+++ components/health_component.gd
@@ -0,0 +1,43 @@
+class_name HealthComponent
+extends Node
+
+signal got_hit()
+signal health_dropped()
+signal health_depleted()
+
+@export var invulnerable: bool = false
+@export var health: int = 1
+@export var team: String = ""
+@export var deplete_once: bool = true
+
+var current_health: int = 1
+var has_depleted: bool = false
+
+
+func reset():
+	current_health = health
+	has_depleted = false
+
+
+func _ready() -> void:
+	reset()
+
+
+func take_damage(damage: DamageParameters) -> void:
+
+	if current_health == 0 and deplete_once and has_depleted:
+		return
+
+	got_hit.emit()
+
+	if not invulnerable:
+		current_health -= 1
+		health_dropped.emit()
+
+		if current_health <= 0:
+
+			if deplete_once and has_depleted:
+				return
+
+			health_depleted.emit()
+			has_depleted = true
