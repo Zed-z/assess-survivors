2113279fd74f24364305f42150ce7fea7de033bf
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-09-27 14:23:13 +0200

    Grass shader and stuff

--- objects/player/projectile_shooter.gd
+++ objects/player/projectile_shooter.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 @export var projectile: PackedScene
+@export var projectile_speed: float = 200
 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
-			var direction: Vector2 = get_global_mouse_position() - global_position
+			var aim_direction: Vector2 = (get_global_mouse_position() - global_position)
+			var direction: Vector2 = aim_direction.normalized() * projectile_speed
+
 			shoot_projectile(direction)
 
 
