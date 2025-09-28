6e1cedc82570b36a4923d6567fc074a5ba49feaf
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-09-28 02:36:36 +0200

    Scene manager tweaks

--- globals/scene_manager/scene_manager.gd
+++ globals/scene_manager/scene_manager.gd
@@ -11,28 +11,31 @@ var uids: Dictionary[String, String] = {
 var current_scene: String = uids.keys()[0]
 var next_scene: String = ""
 
-var load_ok: bool = false
-var currently_loading: String = ""
+var currently_loading: bool = false
+var currently_loaded_scene: String = ""
 
 
 func change_scene(
 	scene: String
 ) -> void:
-	currently_loading = uids[scene]
-	ResourceLoader.load_threaded_request(currently_loading)
+	currently_loaded_scene = uids[scene]
+	ResourceLoader.load_threaded_request(currently_loaded_scene)
 	%SceneTransition/AnimationPlayer.play("fade_out")
 
 
 func _on_scene_transition_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "fade_out":
 		get_tree().paused = true
-		load_ok = true
+		currently_loading = true
 
 
 func _physics_process(_delta: float) -> void:
-	if load_ok and ResourceLoader.load_threaded_get_status(currently_loading) == ResourceLoader.THREAD_LOAD_LOADED:
-		var scene: PackedScene = ResourceLoader.load_threaded_get(currently_loading)
-		get_tree().change_scene_to_packed(scene)
-		%SceneTransition/AnimationPlayer.play("fade_in")
-		get_tree().paused = false
-		load_ok = false
+	if currently_loading:
+		var status := ResourceLoader.load_threaded_get_status(currently_loaded_scene)
+
+		if status == ResourceLoader.THREAD_LOAD_LOADED:
+			var scene: PackedScene = ResourceLoader.load_threaded_get(currently_loaded_scene)
+			get_tree().change_scene_to_packed(scene)
+			%SceneTransition/AnimationPlayer.play("fade_in")
+			get_tree().paused = false
+			currently_loading = false
