6345c7e0bc58939c1d3f46b7d3b773944edf47a7
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-10-30 10:34:00 +0100

    More fixes

--- objects/ui/settings_menu/button_nh.gd
+++ objects/ui/settings_menu/button_nh.gd
@@ -2,5 +2,5 @@ extends Button
 
 
 func _on_pressed() -> void:
-	TranslationServer.set_locale("nh")
-	SettingsManager.set_setting("language", "nh")
+	TranslationServer.set_locale("ja")
+	SettingsManager.set_setting("language", "ja")
