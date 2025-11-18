6a87dadc69d70350a651cacd15845168c3ff695c
igorekkedziorek <126686402+igorekkedziorek@users.noreply.github.com>
2025-11-18 20:48:19 +0100

    add choice panel card

--- objects/ui/choice_panel/choice_panel_choice.gd
+++ objects/ui/choice_panel/choice_panel_choice.gd
@@ -1,15 +1,5 @@
-extends PanelContainer
+extends ChoicePanelCard
 class_name ChoicePanelChoice
-
-signal chosen()
-@export var criterion: AssessCriterion
-@export var choice: Lottery
-@export var disabled: bool:
-	set(val):
-		disabled = val
-		%Button.disabled = disabled
-
-@export_range(0, 360, 0.1, "radians_as_degrees") var lottery_roll_speed: float = 0
 
 
 func setup(_criterion: AssessCriterion, _choice: SingleLottery):
@@ -31,17 +21,3 @@ func setup(_criterion: AssessCriterion, _choice: SingleLottery):
 
 	%SureIcon.texture = criterion.icon
 	%LotteryIcon.texture = criterion.icon
-
-
-func _ready() -> void:
-	disabled = disabled
-
-
-func _physics_process(delta: float) -> void:
-	if lottery_roll_speed > 0:
-		%LotteryCursor.rotation_degrees = lottery_roll_speed * 20 + sin(deg_to_rad(Engine.get_physics_frames()) * 20) * 10
-		%LotteryProgress.rotation += lottery_roll_speed
-
-
-func _on_button_pressed() -> void:
-	chosen.emit()
--- objects/ui/choice_panel_weight/choice_panel_weight.gd
+++ objects/ui/choice_panel_weight/choice_panel_weight.gd
@@ -1,16 +1,7 @@
-extends PanelContainer
+extends ChoicePanelCard
 class_name ChoicePanelWeight
 
-signal chosen()
-
-@export var choice: Lottery
-@export var disabled: bool:
-	set(val):
-		disabled = val
-		%Button.disabled = disabled
-
-@export var item_display: PackedScene
-@export_range(0, 360, 0.1, "radians_as_degrees") var lottery_roll_speed: float = 0
+@export var item_display: PackedScene = preload("res://objects/ui/choice_panel_weight/choice_panel_weight_item.tscn")
 
 
 func setup(_criterion: AssessCriterion, _choice: MultiLottery):
@@ -33,17 +24,3 @@ func fill_container(dict: Dictionary[AssessCriterion, float], container: BoxCont
 		item.criterion = key
 		item.val = dict[key]
 		container.add_child(item)
-
-
-func _ready() -> void:
-	disabled = disabled
-
-
-func _physics_process(delta: float) -> void:
-	if lottery_roll_speed > 0:
-		%LotteryCursor.rotation_degrees = lottery_roll_speed * 20 + sin(deg_to_rad(Engine.get_physics_frames()) * 20) * 10
-		%LotteryProgress.rotation += lottery_roll_speed
-
-
-func _on_button_pressed() -> void:
-	chosen.emit()
