3332e627d28167147a8d809e70b8a3bdf15c3d48
igorekkedziorek <126686402+igorekkedziorek@users.noreply.github.com>
2025-11-19 19:20:53 +0100

    add probability to weight panel

--- objects/ui/choice_panel_weight/choice_panel_weight.gd
+++ objects/ui/choice_panel_weight/choice_panel_weight.gd
@@ -2,6 +2,7 @@ extends ChoicePanelCard
 class_name ChoicePanelWeight
 
 @export var item_display: PackedScene = preload("res://objects/ui/choice_panel_weight/choice_panel_weight_item.tscn")
+var prob_display: Label
 
 
 func setup(_criterion: AssessCriterion, _choice: MultiLottery):
@@ -10,18 +11,29 @@ func setup(_criterion: AssessCriterion, _choice: MultiLottery):
 
 	if (_choice.win_probability == 1):
 		%SureOption.visible = true
+		%SureContainer.visible = true
+		%LotteryContainer.visible = false
 		%Lottery.visible = false
 		%Button.text = tr("CHOICE_PANEL_SAFE")
-		fill_container(_choice.win_array, %SureContainer)
+		fill_container(_choice.win_array, 1, %SureContainer)
 	else:
 		%SureOption.visible = false
 		%Lottery.visible = true
+		%SureContainer.visible = false
+		%LotteryContainer.visible = true
 		%Button.text = tr("CHOICE_PANEL_SPIN")
-		fill_container(_choice.win_array, %WinContainer)
-		fill_container(_choice.loss_array, %LossContainer)
+		fill_container(_choice.win_array,_choice.win_probability, %WinContainer)
+		fill_container(_choice.loss_array,_choice.loss_probability, %LossContainer)
 
 
-func fill_container(dict: Dictionary[AssessCriterion, float], container: BoxContainer) -> void:
+func fill_container(dict: Dictionary[AssessCriterion, float], probability: float, container: BoxContainer) -> void:
+	if probability >= 1:
+		pass
+	else:
+		var prob_label: Label = Label.new()
+		prob_label.text = "CHANCE: " + str(probability*100).pad_decimals(2)
+		container.add_child(prob_label)
+
 	for key in dict:
 		var item = item_display.instantiate()
 		item.criterion = key
