a9c27d8574d681e4a3aa0214061bc13461045172
shamotte <123325697+shamotte@users.noreply.github.com>
2025-12-06 22:32:57 +0100

    Update projectile_enemy.gd

--- objects/projectile/projectile_enemy.gd
+++ objects/projectile/projectile_enemy.gd
@@ -5,3 +5,4 @@ class_name ProjectileEnemy
 func _on_area_entered(area: Area2D) -> void:
 	if area is BaseHurtbox and area is not EnemyHurtbox:
 		area.got_hit(DamageParameters.new(damage))
+		queue_free()
