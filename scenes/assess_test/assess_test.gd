04da984a1f84977a99dd1f3d098b4e454381e4a0
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-12-11 13:17:50 +0100

    More assess stuff

--- scenes/assess_test/assess_test.gd
+++ scenes/assess_test/assess_test.gd
@@ -3,3 +3,8 @@ extends Control
 
 func _on_button_question_pressed() -> void:
 	add_child(GlobalInfo.assess_manager.init_choice_panel())
+
+
+func _on_button_weights_pressed() -> void:
+	%ButtonWeights.disabled = true
+	GlobalInfo.assess_manager.is_weight_phase = true
