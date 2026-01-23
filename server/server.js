d1db83ef70b60a54be42c17de39f2eea246406b6
Zed-z <71879051+Zed-z@users.noreply.github.com>
2026-01-24 00:31:17 +0100

    CORS

--- server/server.js
+++ server/server.js
@@ -11,6 +11,31 @@ const app = express()
 	.use(express.urlencoded({ extended: true, limit: config.dataLimit}))
 	.use(express.json());
 
+// CORS
+const ALLOWED_ORIGINS = [
+    "https://zedgame.itch.io", 
+    "http://localhost:3001", 
+    "http://127.0.0.1:3001"
+];
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+
+    if (ALLOWED_ORIGINS.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
+
 // Load and apply routes from the requests/ folder
 const loadRoutes = (app, routesDir) => {
 	fs.readdirSync(routesDir).forEach(file => {
