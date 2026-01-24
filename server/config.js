03af4d71088627ec6954305b3f5b91857e901bd4
Zed-z <71879051+Zed-z@users.noreply.github.com>
2026-01-24 18:00:45 +0100

    Web issue fixes

--- server/config.js
+++ server/config.js
@@ -3,8 +3,11 @@ module.exports = {
 	port: 3001,
 
 	allowedOrigins: [
-		"https://zedgame.itch.io", 
-		"http://localhost:3001", 
+		"https://zedgame.itch.io",
+		"https://itch.io",
+		"https://html.itch.zone",
+		"https://zedgame.xyz",
+		"http://localhost:3001",
 		"http://127.0.0.1:3001"
 	],
 
