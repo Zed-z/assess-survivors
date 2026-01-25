d3eed7986e4410ce27b4f44413a3decfe55f9d07
igorekkedziorek <126686402+igorekkedziorek@users.noreply.github.com>
2026-01-25 19:06:36 +0100

    end screen better

--- globals/global_info/global_info.gd
+++ globals/global_info/global_info.gd
@@ -10,15 +10,14 @@ var game_camera: GameCamera
 var gameplay_scene: GameplayScene
 
 var game_time: float
-const assess_all = AssessSummary.MOST_RISKY | AssessSummary.LEAST_RISKY | AssessSummary.MOST_WEIGHT | AssessSummary.LEAST_WEIGHT | AssessSummary.QUESTION_ANSWERED_TOTAL | AssessSummary.WEIGHT_QUESTION_ANSWERED_TOTAL
+const assess_all = AssessSummary.MOST_RISKY | AssessSummary.LEAST_RISKY | AssessSummary.MOST_WEIGHT | AssessSummary.LEAST_WEIGHT
 const game_all = GameSummary.GAMETIME | GameSummary.SCORETOTAL
 enum AssessSummary {
 	MOST_RISKY = 1 << 0,
 	LEAST_RISKY = 1 << 1,
 	MOST_WEIGHT = 1 << 2,
 	LEAST_WEIGHT = 1 << 3,
-	QUESTION_ANSWERED_TOTAL = 1 << 4,
-	WEIGHT_QUESTION_ANSWERED_TOTAL = 1 << 5
+	QUESTION_ANSWERED_TOTAL = 1 << 4
 	}
 enum GameSummary {
 	GAMETIME = 1 << 0,
@@ -35,15 +34,14 @@ func write_summary(assess_summary: int, game_summary: int, smart_mode: bool):
 	var assess_weights: Array[float] = []
 	var assess_risks: Array[float] = []
 	var question_answered: int = 0
-	var weight_question_answered: int = 0
 
 	for c in assess_manager.criteria:
 		assess_weights.append(c.weight)
 		assess_risks.append(c.risk_factor)
 		question_answered += c.METRIC_question_answered
 
-	print(assess_weights)
-	print(assess_risks)
+	question_answered += assess_manager.METRIC_weight_question_count
+
 	if smart_mode:
 		if abs(assess_weights.min() - assess_weights.max()) < 0.01:
 			assess_summary ^= AssessSummary.MOST_WEIGHT | AssessSummary.LEAST_WEIGHT
@@ -53,13 +51,13 @@ func write_summary(assess_summary: int, game_summary: int, smart_mode: bool):
 
 	#TODO translate
 	if has_flag(assess_summary, AssessSummary.MOST_RISKY):
-		var most_risky = assess_manager.criteria.find_custom(func(x): return x.risk_factor == assess_risks.max())
+		var most_risky = assess_manager.criteria.find_custom(func(x): return x.risk_factor == assess_risks.min())
 		return_string += "Your most risky stat: "
 		return_string += tr(assess_manager.criteria[most_risky].criterion_name)
 		return_string += "\n"
 
 	if has_flag(assess_summary, AssessSummary.LEAST_RISKY):
-		var least_risky = assess_manager.criteria.find_custom(func(x): return x.risk_factor == assess_risks.min())
+		var least_risky = assess_manager.criteria.find_custom(func(x): return x.risk_factor == assess_risks.max())
 		return_string += "Your least risky stat: "
 		return_string += tr(assess_manager.criteria[least_risky].criterion_name)
 		return_string += "\n"
@@ -81,15 +79,10 @@ func write_summary(assess_summary: int, game_summary: int, smart_mode: bool):
 		return_string += str(question_answered)
 		return_string += "\n"
 
-	if has_flag(assess_summary, AssessSummary.WEIGHT_QUESTION_ANSWERED_TOTAL):
-		return_string += "Total answered (2nd phase): "
-		return_string += str(assess_manager.METRIC_weight_question_count)
-		return_string += "\n"
-
 	if has_flag(game_summary, GameSummary.GAMETIME):
 		return_string += "Your game time: "
-		return_string += str(game_time)
-		return_string += "\n"
+		return_string += "%0.2f" % game_time
+		return_string += "s\n"
 
 	if has_flag(game_summary, GameSummary.SCORETOTAL):
 		return_string += "Your score: "
