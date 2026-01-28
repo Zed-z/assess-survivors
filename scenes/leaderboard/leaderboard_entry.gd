b48fad84d2e9b7d91412c3ae4f5f6adadb656184
igorekkedziorek <126686402+igorekkedziorek@users.noreply.github.com>
2026-01-28 22:23:59 +0100

    yeah

--- scenes/leaderboard/leaderboard_entry.gd
+++ scenes/leaderboard/leaderboard_entry.gd
@@ -6,5 +6,5 @@ func setup(data) -> void:
 	%Rank.text = "#" + str(int(data["rank"]))
 	%PlayerName.text = data["player"]
 	%Score.text = tr("END_SCREEN_SCORE_COUNTER") % int(data["score"])
-	%Riskiness.text = tr("SUMMARY_ONLINE_MEAN_RISKINESS") % data["average_riskiness"]
+	%Riskiness.text = tr("SUMMARY_ONLINE_MEAN_RISKINESS") % float(data["average_riskiness"])
 	%MostWeightStat.text = tr("SUMMARY_MOST_WEIGHED_STAT") % data["most_weight_stat"]
