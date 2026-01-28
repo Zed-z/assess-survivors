8b2f78252b6c395dab8b78439c83e833668054d8
Zed-z <71879051+Zed-z@users.noreply.github.com>
2026-01-28 16:14:13 +0100

    Improved and translated summary

--- globals/global_info/global_info.gd
+++ globals/global_info/global_info.gd
@@ -16,15 +16,14 @@ const assess_all: int = AssessSummary.MOST_RISKY |\
  AssessSummary.MOST_WEIGHT |\
  AssessSummary.LEAST_WEIGHT
 
-const game_all: int = GameSummary.GAMETIME |\
- GameSummary.SCORETOTAL
+const game_all: int = GameSummary.GAMETIME
 
-const online_all: int =	OnlineSummary.BETTER_THAN_SCORE |\
- OnlineSummary.BETTER_THAN_PERCENTILE |\
- OnlineSummary.AVG_RISK |\
- OnlineSummary.RISKIER_THAN |\
- OnlineSummary.CAUTIOUS_THAN |\
- OnlineSummary.RISK_PERCINTILE
+const online_all: int =\
+	OnlineSummary.BETTER_THAN_PERCENTILE |\
+	OnlineSummary.AVG_RISK |\
+	OnlineSummary.RISKIER_THAN_PERCENT |\
+	OnlineSummary.LESS_RISKY_THAN_PERCENT |\
+	OnlineSummary.RANK
 
 const flag_none: int = 0
 
@@ -37,16 +36,15 @@ enum AssessSummary {
 	}
 enum GameSummary {
 	GAMETIME = 1 << 0,
-	SCORETOTAL = 1 << 1
 }
 
 enum OnlineSummary {
 	BETTER_THAN_SCORE = 1 << 0,
 	BETTER_THAN_PERCENTILE = 1 << 1,
 	AVG_RISK = 1 << 2,
-	RISKIER_THAN = 1 << 3,
-	CAUTIOUS_THAN = 1 << 4,
-	RISK_PERCINTILE = 1 << 5
+	RISKIER_THAN_PERCENT = 1 << 3,
+	LESS_RISKY_THAN_PERCENT = 1 << 4,
+	RANK = 1 << 5,
 }
 
 
@@ -77,90 +75,54 @@ func write_summary(assess_summary: int, game_summary: int, smart_mode: bool = tr
 	#TODO translate
 	if has_flag(assess_summary, AssessSummary.MOST_RISKY):
 		var most_risky = assess_manager.criteria.find_custom(func(x): return x.risk_factor == assess_risks.min())
-		return_string += "Your most risky stat: "
-		return_string += tr(assess_manager.criteria[most_risky].criterion_name)
-		return_string += "\n"
+		return_string += tr("SUMMARY_MOST_RISKY_STAT") % tr(assess_manager.criteria[most_risky].criterion_name) + "\n"
 
 	if has_flag(assess_summary, AssessSummary.LEAST_RISKY):
 		var least_risky = assess_manager.criteria.find_custom(func(x): return x.risk_factor == assess_risks.max())
-		return_string += "Your least risky stat: "
-		return_string += tr(assess_manager.criteria[least_risky].criterion_name)
-		return_string += "\n"
+		return_string += tr("SUMMARY_LEAST_RISKY_STAT") % tr(assess_manager.criteria[least_risky].criterion_name) + "\n"
 
 	if has_flag(assess_summary, AssessSummary.MOST_WEIGHT):
 		var most_weight = assess_manager.criteria.find_custom(func(x): return x.weight == assess_weights.max())
-		return_string += "Your most weighted stat: "
-		return_string += tr(assess_manager.criteria[most_weight].criterion_name)
-		return_string += "\n"
+		return_string += tr("SUMMARY_MOST_WEIGHED_STAT") % tr(assess_manager.criteria[most_weight].criterion_name) + "\n"
 
 	if has_flag(assess_summary, AssessSummary.LEAST_WEIGHT):
 		var least_weight = assess_manager.criteria.find_custom(func(x): return x.weight == assess_weights.min())
-		return_string += "Your least weighted stat: "
-		return_string += tr(assess_manager.criteria[least_weight].criterion_name)
-		return_string += "\n"
+		return_string += tr("SUMMARY_LEAST_WEIGHED_STAT") % tr(assess_manager.criteria[least_weight].criterion_name) + "\n"
 
 	if has_flag(assess_summary, AssessSummary.QUESTION_ANSWERED_TOTAL):
-		return_string += "Total answered (1st phase): "
-		return_string += str(question_answered)
-		return_string += "\n"
+		return_string += tr("SUMMARY_ANSWER_COUNT") % question_answered + "\n"
 
 	if has_flag(game_summary, GameSummary.GAMETIME):
-		return_string += "Your game time: "
-		return_string += float_to_time_string(game_time)
-		return_string += "\n"
-
-	if has_flag(game_summary, GameSummary.SCORETOTAL):
-		return_string += "Your score: "
-		return_string += str(score_manager.score)
-		return_string += "\n"
+		return_string += tr("SUMMARY_GAME_TIME") % float_to_time_string(game_time) + "\n"
 
 	return return_string
 
 
 func float_to_time_string(time: float) -> String:
-	var time_string = ""
-
-	if (time / 60.0) >= 1:
-		time_string = str(floor(time / 60.0)) + "m " + "0.2f" % (time - (floor(time / 60.0) * 60)) + "s"
-	else:
-		time_string = 	"%0.2f" % time + "s"
-
-	return time_string
+	var _min: float = floor(time / 60.0)
+	var _sec: float = int(floor(time)) % 60
+	return "%02d:%02d" % [_min, _sec]
 
 
 func append_online(online_summary: int, response) -> String:
 	var return_string = ""
 
+	if has_flag(online_summary, OnlineSummary.RANK):
+		return_string += tr("SUMMARY_ONLINE_RANK") % response["rank"] + "\n"
+
 	if has_flag(online_summary, OnlineSummary.BETTER_THAN_SCORE):
-		return_string += "you were better than: "
-		return_string += str(int(response["less_score_count"]))
-		return_string += " of players\n"
+		return_string += tr("SUMMARY_ONLINE_BETTER_THAN_COUNT") % response["less_score_count"] + "\n"
 
 	if has_flag(online_summary, OnlineSummary.BETTER_THAN_PERCENTILE):
-		return_string += "you were in top: "
-		return_string += "%0.2f" % (response["score_percentile"] * 100)
-		return_string += "%"
-		return_string += " of players\n"
+		return_string += tr("SUMMARY_ONLINE_BETTER_THAN_PERCENT") % (response["score_percentile"] * 100) + "\n"
 
 	if has_flag(online_summary, OnlineSummary.AVG_RISK):
-		return_string += "Your mean risk was: "
-		return_string += str(response["avgerage_riskiness"])
-		return_string += "\n"
+		return_string += tr("SUMMARY_ONLINE_MEAN_RISKINESS") % response["avgerage_riskiness"] + "\n"
 
-	if has_flag(online_summary, OnlineSummary.RISKIER_THAN):
-		return_string += "You were riskier than: "
-		return_string += str(int(response["more_riskiness_count"]))
-		return_string += " of players\n"
+	if has_flag(online_summary, OnlineSummary.RISKIER_THAN_PERCENT):
+		return_string += tr("SUMMARY_ONLINE_RISKIER_THAN_PERCENT") % response["more_riskiness_percentile"] + "\n"
 
-	if has_flag(online_summary, OnlineSummary.CAUTIOUS_THAN):
-		return_string += "You were more cautious than: "
-		return_string += str(int(response["less_riskiness_count"]))
-		return_string += " of players\n"
-
-	if has_flag(online_summary, OnlineSummary.RISK_PERCINTILE):
-		return_string += "You were in top: "
-		return_string += "%0.2f" % (response["riskiness_percentile"] * 100)
-		return_string += "%"
-		return_string += " of risky players\n"
+	if has_flag(online_summary, OnlineSummary.LESS_RISKY_THAN_PERCENT):
+		return_string += tr("SUMMARY_ONLINE_LESS_RISKY_THAN_PERCENT") % response["less_riskiness_percentile"] + "\n"
 
 	return return_string
